178c27a81836f6db875644b0d70b8e056cc3ea76
Hosu <sogeumhosu@gmail.com>
2023-09-14 22:56:58 -0700

    sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,46 +9,21 @@ const pool = new Pool({
   database: 'bootcampx',
 });
 
-// Test the database connection
-// pool.query('SELECT NOW()', (err, res) => {
-//   if (err) {
-//     console.error('Error connecting to the database:', err);
-//   } else {
-//     console.log('Connected to the database');
-//     console.log('Current date and time from the database:', res.rows[0].now);
-//   }
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-//   // Close the database connection
-//   pool.end();
-// });
-
-// pool
-//   .query(
-//     `
-//   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-//   FROM students
-//   JOIN cohorts ON cohorts.id = cohort_id
-//   LIMIT 5;
-//   `
-//   )
-//   .then((res) => {
-//     res.rows.forEach((user) => {
-//       console.log(
-//         `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
-//       );
-//     });
-//   });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
 pool
-  .query(
-    `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
